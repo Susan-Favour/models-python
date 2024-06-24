0f8ccb3559fb81ad61a42ba4f6422f807659c2f7
SusanFavour <susanfavourodongo@gmail.com>
2024-06-24 22:36:49 +0300

    add changes

--- student/models.py
+++ student/models.py
@@ -3,16 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    email = models.EmailField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
     date_of_birth = models.DateField()
-    code = models.PositiveSmallIntegerField()
-    country = models.CharField(max_length=28)
-    gender = models.CharField(max_length=6)
-    bio = models.TextField()
-    id_number = models.IntegerField()
-    grade_level = models.IntegerField()
+    id = models.AutoField(primary_key=True)
+    code = models.PositiveSmallIntegerField(unique=True)
+    gender = models.CharField(max_length=10)
+    country = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+    enrollment_date = models.DateField()
+    guardian_phone_number = models.CharField(max_length=15)
+    guardian_name = models.CharField(max_length=100)
+    picture = models.ImageField()
+    
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
